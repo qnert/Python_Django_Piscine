db6f8c3d6328d050499394a2f2615a3c5cb3622a
Simon Kunert <skunert@1-E-19.42heilbronn.de>
2024-04-11 16:40:04 +0200

    added error management for ex02

--- d03/ex02/request_wikipedia.py
+++ d03/ex02/request_wikipedia.py
@@ -10,9 +10,16 @@ def main(search: str):
     "explaintext": True
   }
   request = requests.get(url, params)
+  if request.status_code != 200:
+    print("Error: couldn't get information from api.")
+    return 1
   data = request.json()
   id = next(iter(data["query"]["pages"]))
-  page_content = data["query"]["pages"][id]["extract"]
+  try:
+    page_content = data["query"]["pages"][id]["extract"]
+  except:
+    print("Error: searched site was no found.")
+    return 1
   with open(search + ".wiki", "w") as f:
     f.write(page_content)
     f.close()
