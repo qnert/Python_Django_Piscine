87c514c3491c944993ece53f777919c64245b106
qnert <qnertsimon@gmail.com>
2024-04-16 11:48:35 +0200

    added ex03 with little help but works fine now

--- d03/ex03/roads_to_philosophy.py
+++ d03/ex03/roads_to_philosophy.py
@@ -1,18 +1,37 @@
 import requests, sys
 from bs4 import BeautifulSoup
 
-def main(search: str):
-  url = "https://de.wikipedia.org/wiki/"
-  request = requests.get(url + search)
+def wiki_crawl(search: str, history: set) -> int:
+  url = "https://en.wikipedia.org" + search
+  request = requests.get(url)
   if request.status_code != 200:
-    print("Error: couldn't get information from wikipedia.")
-    return 1
-  start_pos = int(str(request._content).find("<title>") + len("<title>"))
-  end_pos = str(request._content).find(" ", start_pos)
-  title = str(request._content)[start_pos:end_pos]
+    print("It leads to a dead end! So it took 0", end = '')
+    return
+  soup = BeautifulSoup(request.content, 'html.parser')
+  title = soup.select_one("#firstHeading").text
   print(title)
+  if title != "Philosophy":
+    content = soup.find(id='mw-content-text')
+    links = content.select('p > a')
+    for link in links:
+      if link.get('href') in history:
+        print('It leads to an infinite loop! So it took 0', end = '')
+        return
+      elif link.get('href') is not None and link['href'].startswith('/wiki/')\
+            and all(c != ':' for c in link['href']):
+        history.add(search)
+        wiki_crawl(link['href'], history)
+        return
+      else:
+        return
+  else:
+    print("It took " + str(len(history)), end = '')
+    return
+  print("It couldn't be resolved within any amount of", end = '')
 
-
+def main(search: str):
+  wiki_crawl("/wiki/" + search, set())
+  print(" tries to get from " + search + " to Philosophy")
 
 
 if __name__ == "__main__":
@@ -20,3 +39,4 @@ if __name__ == "__main__":
     print("Error: wrong amount of arguments.")
   else:
     main(sys.argv[1])
+
